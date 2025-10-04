86cb85cee5a7825aae47df10958ef9597b5b0bde
Aigiz <iiiaigiziii@gmail.com>
2025-10-04 13:09:10 +0300

    Update dir database: add database.db file, add async sqlalchemy orm

--- src/database/base.py
+++ src/database/base.py
@@ -1,7 +1,11 @@
-books_ids = 1
-books_db = {
-    1: {
-        "autors": ["Mark"],
-        "title": "Hello world"
-    },
-}
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+URL = "sqlite+aiosqlite:///database.db"
+
+async_engine = create_async_engine(url=URL, echo=True)
+async_session_fabric = async_sessionmaker(bind=async_engine, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
